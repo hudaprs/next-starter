d3c55d859a28f2cec281881af4c85ad88e143b3c
Huda Prasetyo <real.hudaprasetyo@gmail.com>
2024-06-25 00:32:41 +0700

    perf(eslint): enhance

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-// Tailwind
-import type { Config } from 'tailwindcss'
 import { fontFamily } from 'tailwindcss/defaultTheme'
+
+import type { Config } from 'tailwindcss'
 
 const config = {
 	darkMode: ['class'],
